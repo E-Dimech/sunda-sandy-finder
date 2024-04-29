60ce27da1d7fd73601f8e2497429b0c54b252f95
Eric Dimech <ericdimech@hotmail.com>
2024-04-29 09:55:45 -0400

    Update serverand mapRoutes to now get markers data from mongo instead of hardcoded dataset

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+const mapRoutes = require("./mapRoutes");
 const app = express();
 const port = 8000;
 
-const mapRoutes = require("./mapRoutes");
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Middleware
+app.use(express.json());
+
+// Routes
 app.use("/api", mapRoutes);
 
 app.get("/", (req, res) => {
